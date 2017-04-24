44258eccaf820d6513cc2b7b20577df2a27333bd
jsdtwry <jsdtwry@163.com>
2017-04-25 05:20:12 +0800

    add wav-to-cluster

---
+++ server/compute_feat.py
@@ -0,0 +1,82 @@
+from initial_seg import *
+from spk_cluster_reseg import *
+import sys
+import os
+
+wav_name = sys.argv[1]
+conf = sys.argv[2]
+nnet = sys.argv[3]
+
+def feat_extract(wav_name, conf, nnet):
+    utt_name = wav_name.split('/')[-1].split('.')[0]
+    # mkdir
+    os.makedirs('feat/'+utt_name)
+    # generate scp
+    fout = file('feat/'+utt_name+'/wav.scp','w')
+    fout.write(utt_name+' '+sys.path[0]+'/'+wav_name)
+    fout.close()
+    # feat_compute.sh
+    # os.system('bash compute_feat.sh feat/'+utt_name+' '+conf+' '+nnet)
+    data = 'feat/'+utt_name
+
+    os.system('compute-fbank-feats --config='+conf+'/fbank.conf scp:'+data+'/wav.scp ark,t,scp:'+data+'/fbank_feats.ark,'+data+'/fbank_feats.scp')
+    os.system('compute-vad --config='+conf+'/vad.conf scp:'+data+'/fbank_feats.scp ark,t,scp:'+data+'/fbank_vad.ark,'+data+'/fbank_vad.scp')
+    os.system('nnet3-compute --use-gpu=no '+nnet+'/final.last_hid.raw ark:'+data+'/fbank_feats.ark ark,t,scp:'+data+'/dvector.ark,'+data+'/dvector.scp')
+
+def dvec_seg(wav_name):
+    utt_name = wav_name.split('/')[-1].split('.')[0]
+    dvector_file = 'feat/'+utt_name+'/dvector.ark' # 400 dim
+    vad_file = 'feat/'+utt_name+'/fbank_vad.ark'
+    
+    # load d-vector feature and vad result
+    utt_lable, feat_content = readfeatfromkaldi(dvector_file, 400)
+    vad_utt_label, vad_content = readvadfromkaldi(vad_file)
+    
+    # remove silence regions
+    feat_vad, feat_time = gen_feat_vad(feat_content, vad_content)
+    
+    # initial segmentation
+    scores, times, det_time, det_index = initial_seg(feat_vad, feat_time, 0.1, 0.01, 'dvec')
+
+    # k-means clustering from initial segmentation
+    change_point, segment_result, spk_model = spk_k_means_cluster(det_index, feat_vad, feat_time, 'svm')
+
+    # transform index of features to time points
+    det_time = [frame2time(feat_time[i], mfcc_shift) for i in change_point]
+    cluster_result = [[[frame2time(feat_time[i[0][0]], mfcc_shift), frame2time(feat_time[i[0][1]], mfcc_shift)], i[1]] for i in segment_result]
+    
+    print cluster_result
+  
+def dvec_seg_one_model(wav_name, start, end):
+    utt_name = wav_name.split('/')[-1].split('.')[0]
+    dvector_file = 'feat/'+utt_name+'/dvector.ark' # 400 dim
+    vad_file = 'feat/'+utt_name+'/fbank_vad.ark'
+    
+    # load d-vector feature and vad result
+    utt_lable, feat_content = readfeatfromkaldi(dvector_file, 400)
+    vad_utt_label, vad_content = readvadfromkaldi(vad_file)
+    
+    # remove silence regions
+    feat_vad, feat_time = gen_feat_vad(feat_content, vad_content)
+    
+    # initial segmentation
+    scores, times, det_time, det_index = initial_seg(feat_vad, feat_time, 0.1, 0.01, 'dvec')
+
+    # k-means clustering from initial segmentation
+    #change_point, segment_result, spk_model = spk_k_means_cluster(det_index, feat_vad, feat_time, 'svm')
+    spk_one = np.mean(feat_content[int(start*100):int(end*100)], axis=0)
+    #print len(spk_one)
+    #spk_model = get_spk_model(feat_content, 'F001HJN_F002VAN_001')
+    change_point, segment_result = spk_reseg_with_one_model(det_index, feat_vad, feat_time, spk_one)
+    # transform index of features to time points
+    det_time = [frame2time(feat_time[i], mfcc_shift) for i in change_point]
+    cluster_result = [[[frame2time(feat_time[i[0][0]], mfcc_shift), frame2time(feat_time[i[0][1]], mfcc_shift)], i[1]] for i in segment_result]
+    
+    print cluster_result
+    
+
+
+#feat_extract(wav_name, conf, nnet)
+dvec_seg(wav_name)
+dvec_seg_one_model(wav_name, 0.21, 1.50)
+
---
+++ server/spk_cluster_reseg.py
@@ -0,0 +1,151 @@
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn import svm
+from initial_seg import *
+
+def gen_block_representation(det_index, feat_vad, feat_time):
+    start_index = 0
+    block_rep = []
+    block_tag = []
+    for i in det_index:
+        block_rep.append(np.mean(feat_vad[start_index:i], axis=0))
+        block_tag.append([start_index, i])
+        start_index = i
+    return block_rep, block_tag
+
+'''
+speaker clustering using k-means, default K=2
+'''
+def spk_k_means_cluster(det_index, feat_vad, feat_time, type):
+    spk0_rep, spk1_rep = [], []
+    cluster_change_point, cluster_result = [], []
+    last_index = 0
+    block_rep, block_tag = gen_block_representation(det_index, feat_vad, feat_time)
+    y_pred = KMeans(n_clusters=2).fit_predict(block_rep)
+    for i, k in enumerate(y_pred):
+        if k==0:
+            for j in range(block_tag[i][0], block_tag[i][1]):
+                spk0_rep.append(feat_vad[j])
+        else:
+            for j in range(block_tag[i][0], block_tag[i][1]):
+                spk1_rep.append(feat_vad[j])
+        if i==0:
+            continue
+        if k!=y_pred[i-1]:
+            cluster_change_point.append(block_tag[i][0])
+            cluster_result.append([[last_index, block_tag[i][0]], k])
+            last_index = block_tag[i][0]
+    if type=='mean': 
+        spk0_model = np.mean(spk0_rep, axis=0)
+        spk1_model = np.mean(spk1_rep, axis=0)
+
+        return cluster_change_point, cluster_result, [spk0_model, spk1_model]
+    else:
+        clf = svm.SVC()
+        clf.fit(block_rep, y_pred)
+        return cluster_change_point, cluster_result, clf
+
+'''
+generate dvector speaker models
+'''
+def get_spk_model(feat, utt_name):
+    reflist = file('../thu_ev_tag/'+utt_name+'.txt').readlines()
+    spk1 = utt_name.split('_')[0][:-3]
+    spk2 = utt_name.split('_')[1][:-3]
+    #print spk1, spk2
+    
+    spk_dict = {spk1:[],spk2:[]}
+    for i in reflist:
+        each = i.split()
+        if each[2]=='SIL' or each[2]=='OVERLAP':
+            continue
+        spk_dict[each[2]].append([time2frame(float(each[0]), mfcc_shift),time2frame(float(each[1]), mfcc_shift)])
+    spk1_data, spk2_data = [], []
+    
+    for i in spk_dict[spk1]:
+        spk1_data += feat[i[0]:i[1]].tolist()
+    for i in spk_dict[spk2]:
+        spk2_data += feat[i[0]:i[1]].tolist()
+    spk0_rep = np.array(spk1_data)
+    spk1_rep = np.array(spk2_data)
+    spk0_model = np.mean(spk0_rep, axis=0)
+    spk1_model = np.mean(spk1_rep, axis=0)
+    return spk0_model, spk1_model
+    
+ 
+'''
+speaker clustering and combination with given speaker models
+'''
+def spk_reseg_with_models(det_index, feat_vad, feat_time, spk_model):
+    cluster_change_point, cluster_result = [], []
+    spk0_model, spk1_model = spk_model
+    last_index = 0
+    block_rep, block_tag = gen_block_representation(det_index, feat_vad, feat_time)
+    y_pred = []
+    for i in block_rep:
+        cos_0 = np.dot(i,spk0_model)/(np.linalg.norm(i)*np.linalg.norm(spk0_model))
+        cos_1 = np.dot(i,spk1_model)/(np.linalg.norm(i)*np.linalg.norm(spk1_model))
+        if cos_0 < cos_1:
+            y_pred.append(0)
+        else:
+            y_pred.append(1)
+    for i, k in enumerate(y_pred):
+        if i==0:
+            continue
+        if k!=y_pred[i-1]:
+            cluster_change_point.append(block_tag[i][0])
+            cluster_result.append([[last_index, block_tag[i][0]], k])
+            last_index = block_tag[i][0]
+    return cluster_change_point, cluster_result
+
+'''speaker clustering and combineation with only one speaker model'''
+def spk_reseg_with_one_model(det_index, feat_vad, feat_time, spk_model):
+    spk0_rep, spk1_rep = [], []
+    cluster_change_point, cluster_result = [], []
+    last_index = 0
+    block_rep, block_tag = gen_block_representation(det_index, feat_vad, feat_time)
+    y_pred = KMeans(n_clusters=2).fit_predict(block_rep)
+    for i, k in enumerate(y_pred):
+        if k==0:
+            for j in range(block_tag[i][0], block_tag[i][1]):
+                spk0_rep.append(feat_vad[j])
+        else:
+            for j in range(block_tag[i][0], block_tag[i][1]):
+                spk1_rep.append(feat_vad[j])
+        if i==0:
+            continue
+        if k!=y_pred[i-1]:
+            cluster_change_point.append(block_tag[i][0])
+            cluster_result.append([[last_index, block_tag[i][0]], k])
+            last_index = block_tag[i][0]
+    spk0_model = np.mean(spk0_rep, axis=0)
+    spk1_model = np.mean(spk1_rep, axis=0)
+
+    cluster_change_point, cluster_result = [], []
+    cos_m0 = np.dot(spk_model,spk0_model)/(np.linalg.norm(spk_model)*np.linalg.norm(spk0_model))
+    cos_m1 = np.dot(spk_model,spk1_model)/(np.linalg.norm(spk_model)*np.linalg.norm(spk1_model))
+    if cos_m0 < cos_m1:
+        spk0_model = spk_model
+    else:
+        spk1_model = spk_model
+    
+    last_index = 0
+    y_pred = []
+    for i in block_rep:
+        cos_0 = np.dot(i,spk0_model)/(np.linalg.norm(i)*np.linalg.norm(spk0_model))
+        cos_1 = np.dot(i,spk1_model)/(np.linalg.norm(i)*np.linalg.norm(spk1_model))
+        if cos_0 < cos_1:
+            y_pred.append(0)
+        else:
+            y_pred.append(1)
+    for i, k in enumerate(y_pred):
+        if i==0:
+            continue
+        if k!=y_pred[i-1]:
+            cluster_change_point.append(block_tag[i][0])
+            cluster_result.append([[last_index, block_tag[i][0]], k])
+            last_index = block_tag[i][0]
+    return cluster_change_point, cluster_result
+
+
+
